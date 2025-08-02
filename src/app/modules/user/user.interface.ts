db08ce0b137591fee05cef2d5550bd40e41f3360
96mdjanealam <96mdjanealam@gmail.com>
2025-08-03 00:07:05 +0600

    user model and interface created

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,27 @@
+import { Types } from "mongoose";
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export enum Role {
+  ADMIN = "ADMIN",
+  USER = "USER",
+}
+
+export interface IUser {
+  _id?: Types.ObjectId;
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  address?: string;
+  isDeleted?: boolean;
+  isActive?: IsActive;
+  parcels?: Types.ObjectId[];
+  role: Role;
+  createdAt?: Date;
+  updatedAt?: Date; 
+}
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+import { IUser, Role, IsActive } from "./user.interface";
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    role: {
+      type: String,
+      enum: Object.values(Role),
+      default: Role.USER,
+    },
+    phone: { type: String },
+    address: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    isActive: {
+      type: String,
+      enum: Object.values(IsActive),
+      default: IsActive.ACTIVE,
+    },
+    parcels: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Parcel",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const User = model<IUser>("User", userSchema);
